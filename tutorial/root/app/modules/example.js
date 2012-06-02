a7b67b001e4fd869d1d08a369ee38d60adbfda55
tbranyen <tim@tabdeveloper.com>
2012-06-02 19:03:28 -0400

    adding in tutorial and todomvc fixes

--- tutorial/root/app/modules/example.js
+++ tutorial/root/app/modules/example.js
@@ -1,40 +1,29 @@
 define([
-  "namespace",
+  "app",
 
   // Libs
-  "use!backbone"
+  "backbone"
 
   // Modules
 
   // Plugins
 ],
 
-function(namespace, Backbone) {
+function(app, Backbone) {
 
   // Create a new module
   var Example = namespace.module();
 
-  // Example extendings
-  Example.Model = Backbone.Model.extend({ /* ... */ });
-  Example.Collection = Backbone.Collection.extend({ /* ... */ });
-  Example.Router = Backbone.Router.extend({ /* ... */ });
-
   // This will fetch the tutorial template and render it.
   Example.Views.Tutorial = Backbone.View.extend({
-    template: "app/templates/example.html",
+    template: "app/templates/example",
 
     render: function(done) {
-      var view = this;
+      // Fetch the template.
+      var tmpl = app.fetchTemplate(this.template);
 
-      // Fetch the template, render it to the View element and call done.
-      namespace.fetchTemplate(this.template, function(tmpl) {
-        view.el.innerHTML = tmpl();
-
-        // If a done function is passed, call it with the element
-        if (_.isFunction(done)) {
-          done(view.el);
-        }
-      });
+      // Set the template contents.
+      this.$el.html(tmpl());
     }
   });
 
