ffdd60ed63f0d273c809cc6233c24cd8e9f9e345
tbranyen <tim@tabdeveloper.com>
2012-06-02 16:31:40 -0400

    removed placeholders set module_name

--- module/root/app/modules/name.js
+++ module/root/app/modules/name.js
@@ -7,12 +7,10 @@ define([
 ],
 
 function(app, Backbone) {
-  var {%= name %} = app.module();
+  var {%= module_name %} = app.module();
 
-  {% if (placeholders) { %}
-    {%= name %}.Model = Backbone.Model.extend({});
-    {%= name %}.Collection = Backbone.Model.extend({});
-  {% } %}
+  {%= module_name %}.Model = Backbone.Model.extend({});
+  {%= module_name %}.Collection = Backbone.Model.extend({});
 
-  return {%= name %};
+  return {%= module_name %};
 });
